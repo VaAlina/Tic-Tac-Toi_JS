d7c1e7d2601e6f3dca6380bca8855f1f8a5f552d
Alina <ananyivaalina@gmail.com>
2016-05-12 21:55:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 # Tic-Tac-Toi_JS
 // It's unfinished.
-//http://jsfiddle.net/VaAlina/c3euan5z/8/
+//http://jsfiddle.net/VaAlina/c3euan5z/11/
 var game = {
     cellsAmount: 5,// Make it incrementable in the future versions of game.
+    playersAmount: 2,
     x_cells:[],// Put here all the moves of x, to get know which cells are empty.
     o_cells:[],
+    allMoves: [],
     x_move: 0, // Last move.
     o_move: 0,
     drawTable: function(){
@@ -25,14 +27,44 @@ var game = {
     changeCellsAmount: function(){
     
     },
-    getX: function(){// Get user input. User plays for "x".
+    init: function(){// Add the event listeners to all cells. Run only ones.
         for(var i = 0; i < game.cellsAmount * game.cellsAmount; i++){
             document.getElementById(i).addEventListener("click", function(){
-                var text = document.createTextNode("x");
-                alert(this.id);
-                this.appendChild(text);
+                if(game.allMoves.length == 0){ // If array with moves is empty, "x" starts.
+                    var text = document.createTextNode("x");
+                    this.appendChild(text);
+                    game.allMoves.push("1");
+                }else if(game.allMoves.length % 2 != 0){// "o" turn
+                    var text = document.createTextNode("o");
+                    this.appendChild(text);
+                    game.allMoves.push("1");
+                }else{// "x" turn
+                    var text = document.createTextNode("x");
+                    this.appendChild(text);
+                    game.allMoves.push("1");
+                }
+                
             });
         }
+    },
+    run: function(){
+        switch(game.playersAmount){
+            case 1: 
+                alert("1 player");
+                break;
+            case 2: 
+                alert("2 players");
+                break;
+          default:
+                alert("2 players");
+            
+        }
+    },
+    getX: function(){
+        // Get user input. User plays for "x".
+    },
+    getO: function(){// Get the second user input if exist.
+    
     },
     calculateO: function(){// Define the best answer for "o".
     
@@ -45,4 +77,5 @@ var game = {
     }
 };
 game.drawTable();
-game.getX();
+game.init();
+game.run();
