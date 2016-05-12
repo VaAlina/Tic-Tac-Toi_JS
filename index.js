ed353518d7074122466d646495c475360d44d499
Alina <ananyivaalina@gmail.com>
2016-05-12 20:34:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 # Tic-Tac-Toi_JS
 // It's unfinished.
-//http://jsfiddle.net/VaAlina/c3euan5z/4/
-
+//http://jsfiddle.net/VaAlina/c3euan5z/8/
 var game = {
     cellsAmount: 5,// Make it incrementable in the future versions of game.
     x_cells:[],// Put here all the moves of x, to get know which cells are empty.
@@ -11,14 +10,15 @@ var game = {
     drawTable: function(){
         var table = document.createElement("TABLE");
         document.body.appendChild(table);
-        for(var i = 0; i < game.cellsAmount; i++){ // Append table rows.
+        for(var i = 0, k = 0; i < game.cellsAmount; i++){ // Append table rows.
             var tr = document.createElement("TR");
             table.appendChild(tr);
-            for(var j = 0; j < game.cellsAmount; j++){ // Append table cells.
+            for(var j = 0; j < game.cellsAmount; j++, k++){ // Append table cells.
                 var td = document.createElement("TD");
                 tr.appendChild(td);
                 var t = document.createTextNode("");
                 td.appendChild(t);
+                td.id = k; // Add id to every cell.
             }
         }   
     },
@@ -26,7 +26,13 @@ var game = {
     
     },
     getX: function(){// Get user input. User plays for "x".
-    
+        for(var i = 0; i < game.cellsAmount * game.cellsAmount; i++){
+            document.getElementById(i).addEventListener("click", function(){
+                var text = document.createTextNode("x");
+                alert(this.id);
+                this.appendChild(text);
+            });
+        }
     },
     calculateO: function(){// Define the best answer for "o".
     
@@ -39,3 +45,4 @@ var game = {
     }
 };
 game.drawTable();
+game.getX();
